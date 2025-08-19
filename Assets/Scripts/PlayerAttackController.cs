9842345ba4b6cc84dbec70bc62e0da3b23b16653
song7446 <songwon4529@naver.com>
2025-08-19 19:49:31 +0900

    [Refactor] 플레이어 공격, 움직임 분리

--- Assets/Scripts/PlayerAttackController.cs
+++ Assets/Scripts/PlayerAttackController.cs
@@ -6,8 +6,55 @@ using UnityEngine.InputSystem;
 
 public class PlayerAttackController : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer playerSR;
+    [SerializeField] private Camera camera;
+    [SerializeField] private SpriteRenderer weaponSR;
+    
+    private Vector2 playerPos;
+    private Vector2 mouseWorldPos;
+
+    
+    private void FixedUpdate()
+    {
+        Look();
+    }
+
     private void OnFire(InputValue inputValue)
     {
         
+    }
+    
+    private void OnLook(InputValue inputValue)
+    {
+        Vector2 mousePos = inputValue.Get<Vector2>();
+        mouseWorldPos = camera.ScreenToWorldPoint(mousePos);
+    }
+    
+    private void Look()
+    {
+        playerPos = transform.position;
+        if (playerPos.x > mouseWorldPos.x)
+        {
+            playerSR.flipX = true;
+            weaponSR.flipY = true;
+        }
+        else
+        {
+            playerSR.flipX = false;
+            weaponSR.flipY = false;       
+        }
+
+        RotateWeapon();
+    }
+
+    private void RotateWeapon()
+    {
+        Vector2 direction = mouseWorldPos - (Vector2)weaponSR.transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        
+        weaponSR.transform.rotation = Quaternion.Euler(0, 0, angle);
+        
+        weaponSR.transform.position = playerPos + direction * 0.1f;
     }
 }
--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -11,26 +11,16 @@ public class PlayerMoveController : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
-
-    private SpriteRenderer playerSR;
-
-    [SerializeField] private Camera camera;
-    private Vector2 playerPos;
-    private Vector2 mouseWorldPos;
-
-    [SerializeField] private SpriteRenderer weaponSR;
-
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         playerAnimController = GetComponent<PlayerAnimController>();
-        playerSR = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         Move();
-        Look();
     }
 
     private void OnMove(InputValue inputValue)
@@ -38,43 +28,9 @@ public class PlayerMoveController : MonoBehaviour
         moveInput = inputValue.Get<Vector2>();
         playerAnimController.OnMoveAnim(moveInput);
     }
-
-    private void OnLook(InputValue inputValue)
-    {
-        Vector2 mousePos = inputValue.Get<Vector2>();
-        mouseWorldPos = camera.ScreenToWorldPoint(mousePos);
-    }
-
+    
     private void Move()
     {
         rb.velocity = moveInput.normalized * moveSpeed;
-    }
-
-    private void Look()
-    {
-        playerPos = transform.position;
-        if (playerPos.x > mouseWorldPos.x)
-        {
-            playerSR.flipX = true;
-            weaponSR.flipY = true;
-        }
-        else
-        {
-            playerSR.flipX = false;
-            weaponSR.flipY = false;       
-        }
-
-        RotateWeapon();
-    }
-
-    private void RotateWeapon()
-    {
-        Vector2 direction = mouseWorldPos - (Vector2)weaponSR.transform.position;
-
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        
-        weaponSR.transform.rotation = Quaternion.Euler(0, 0, angle);
-        
-        weaponSR.transform.position = playerPos + direction * 0.1f;
     }
 }
