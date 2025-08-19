e9d9806d0c5c17b66783d4b2fc425794ef2885ca
song7446 <songwon4529@naver.com>
2025-08-19 21:11:59 +0900

    [Feat] 총알 발사 추가

--- Assets/Scripts/PlayerAttackController.cs
+++ Assets/Scripts/PlayerAttackController.cs
@@ -9,27 +9,46 @@ public class PlayerAttackController : MonoBehaviour
     [SerializeField] private SpriteRenderer playerSR;
     [SerializeField] private Camera camera;
     [SerializeField] private SpriteRenderer weaponSR;
-    
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawnPos;
+
     private Vector2 playerPos;
     private Vector2 mouseWorldPos;
 
-    
+    public float detectionRange = 5f;
+    public float fireDelay = 1f;
+    private float fireTimer = 0f;
+    public LayerMask enemyLayer;
+    public bool isDetecting = true;
+
+    private List<Transform> enemies = new List<Transform>();
+
+    private void Update()
+    {
+        if (isDetecting)
+        {
+            DetectEnemies();
+        }
+    }
+
     private void FixedUpdate()
     {
         Look();
+
+        fireTimer -= Time.deltaTime;
+        if (fireTimer <= 0f)
+        {
+            AutoFire();
+            fireTimer = fireDelay;       
+        }
     }
 
-    private void OnFire(InputValue inputValue)
-    {
-        
-    }
-    
     private void OnLook(InputValue inputValue)
     {
         Vector2 mousePos = inputValue.Get<Vector2>();
         mouseWorldPos = camera.ScreenToWorldPoint(mousePos);
     }
-    
+
     private void Look()
     {
         playerPos = transform.position;
@@ -41,7 +60,7 @@ public class PlayerAttackController : MonoBehaviour
         else
         {
             playerSR.flipX = false;
-            weaponSR.flipY = false;       
+            weaponSR.flipY = false;
         }
 
         RotateWeapon();
@@ -49,12 +68,44 @@ public class PlayerAttackController : MonoBehaviour
 
     private void RotateWeapon()
     {
-        Vector2 direction = mouseWorldPos - (Vector2)weaponSR.transform.position;
+        Vector2 direction = mouseWorldPos - (Vector2)playerPos;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        
         weaponSR.transform.rotation = Quaternion.Euler(0, 0, angle);
         
-        weaponSR.transform.position = playerPos + direction * 0.1f;
+        float weaponOffset = 0.5f;
+        weaponSR.transform.position = playerPos + direction.normalized * weaponOffset;
+    }
+
+    private void DetectEnemies()
+    {
+        enemies.Clear();
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRange, enemyLayer);
+
+        foreach (Collider2D hit in hits)
+        {
+            enemies.Add(hit.transform);
+        }
+    }
+
+    private void AutoFire()
+    {
+        if (enemies.Count > 0)
+        {
+            Vector2 direction = mouseWorldPos - (Vector2)weaponSR.transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            Quaternion bulletAngle = Quaternion.Euler(0, 0, angle - 90);
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPos.position, bulletAngle, null);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.velocity = direction.normalized * 5f;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
     }
 }
