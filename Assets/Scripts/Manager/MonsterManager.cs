6d31ace54281c52d3e0fb12784525d25d09c6017
song7446 <songwon4529@naver.com>
2025-08-20 17:19:25 +0900

    [Feat] 몬스터 자동 생성

--- Assets/Scripts/Manager/MonsterManager.cs
+++ Assets/Scripts/Manager/MonsterManager.cs
@@ -2,31 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
 
 
 public class MonsterManager : MonoBehaviour
 {
     public static MonsterManager Instance;
-    
-    [SerializeField] private Transform monsterSpawnPos;
+
+    [SerializeField] private Tilemap tilemap;
     [SerializeField] private GameObject[] monsterPrefabs;
+
+    private List<Vector2> validPositions = new List<Vector2>();
 
     private void Awake()
     {
         Instance = this;
+
+        foreach (var pos in tilemap.cellBounds.allPositionsWithin)
+        {
+            if (tilemap.HasTile(pos))
+            {
+                Vector2 worldPos = tilemap.CellToWorld(pos) + tilemap.tileAnchor;
+                validPositions.Add(worldPos);
+            }
+        }
     }
 
     private void Start()
     {
-        CreateMonster();
-        // InvokeRepeating("CreateMonster", 1f, 3f);
+        InvokeRepeating("CreateMonster", 1f, 2f);
     }
 
     public void CreateMonster()
     {
         int rand = Random.Range(0, monsterPrefabs.Length);
-        GameObject monster = Instantiate(monsterPrefabs[rand], monsterSpawnPos.position, Quaternion.identity);
+        int randPos = Random.Range(0, validPositions.Count);
+        GameObject monster = Instantiate(monsterPrefabs[rand], validPositions[randPos], Quaternion.identity);
         MonsterStatHandler statHandler = monster.GetComponent<MonsterStatHandler>();
         statHandler.Init(DataManager.Instance.GetRandomMonsterData());
     }
--- Assets/Scripts/Monster/MonsterMoveController.cs
+++ Assets/Scripts/Monster/MonsterMoveController.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterMoveController : MonoBehaviour
 {
-    
+    private void Update()
+    {
+        GameManager.Instance.player.transform.position = transform.position;
+    }
 }
